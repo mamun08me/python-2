cca1812492eb34da078151b5df453997968bc4b8
mamun08me <mamuncuet96@gmail.com>
2026-06-03 00:11:47 +0600

    new changes

--- class_13/list_1.py
+++ class_13/list_1.py
@@ -1,1 +1,24 @@
-listl_of_items=["mamun",12344,True,False,]
+list_of_items=["mamun",12344,True,False,"habijabi"]
+
+'''list_of_items.append("Jahid")
+print(list_of_items)
+
+print(list_of_items[ :-1])
+
+print(list_of_items[2:5])'''
+
+list_of_items.remove("habijabi")
+print(list_of_items)
+
+list_of_items.append("habijabi")
+print(list_of_items)
+
+list_of_items.insert(4,"siddika")
+print(list_of_items)
+
+list_of_items.pop(-1)
+print(list_of_items)
+
+list_of_items[-1]=("Mamun-or-Rashid")
+print(list_of_items)
+
