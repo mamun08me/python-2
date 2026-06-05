e9659bc502631502f90a6a76994d9da1a024a8de
mamun08me <mamuncuet96@gmail.com>
2026-06-05 16:38:57 +0600

    update

--- class-17/sets.py
+++ class-17/sets.py
@@ -1,8 +1,18 @@
-my_set={10, 30, 20}
+my_set={10, 30, 20,10}
+print(my_set)
+#print(my_set[1])#set is not indexed
+my_set.remove(30)#mutable
 print(my_set)
 
+a=
 
-
-#set is unordered
+#set is unordered beacuse it uses hash table 
 #set is not allowed duplicates or unique
-#
+#set is not indexed
+#set is mutable
+
+#difference among list,tuple and set
+
+#list[]------ordered, Duplicate, indexed Mutable 
+#tuple()-----ordered, Duplicate, indexed, Not Mutable
+#Set{}-------only mutable
