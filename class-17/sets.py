7fecaf2a77f559c9beb0a85ce929d2a8b0550770
mamun08me <mamuncuet96@gmail.com>
2026-06-05 17:08:15 +0600

    update

--- class-17/sets.py
+++ class-17/sets.py
@@ -4,7 +4,20 @@ print(my_set)
 my_set.remove(30)#mutable
 print(my_set)
 
-a=
+a={10,20,30,40}
+a.add(50)
+print(a)
+#a.update("mamun")
+#a.update({1,2})
+#a.update([1,2])
+a|={1,2}
+print(a)
+#a.remove(100)
+a.discard(100)
+#a.pop()
+#a.pop()
+print(a)
+
 
 #set is unordered beacuse it uses hash table 
 #set is not allowed duplicates or unique
@@ -16,3 +29,11 @@ a=
 #list[]------ordered, Duplicate, indexed Mutable 
 #tuple()-----ordered, Duplicate, indexed, Not Mutable
 #Set{}-------only mutable
+#index-based methods do not work with sets
+#add() method uses to insert a item somewhere in the set,but only if it is new
+#update() method use for merging another group of values(iterable)into set
+#we can use math operators as quick shortcuts:|&-^
+#|= for update shortcuts
+
+#if you remove missing values which is not in sets there will be error showing instead you can use discard
+#discard use for removing the item if it is exists and does nothing if it does not
